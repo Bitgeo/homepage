5506556000bdf02c7291662f1edf1238e13ad7fa
Maciej Pietsch <bitgeomp@gmail.com>
2019-06-11 06:25:59 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// alert('Siema!');
+// console.log('siema');
+const name = 'Maciej Pietsch';
+const age = 34;
+// let maciek = null;
+
+// console.log(name);
+// console.log(age);
+// console.log(maciek);
+
+// // console.log(`${name}
+// // ${age}`); w dwóch linijkach
+
+// console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+console.log('Witaj na mojej stronie i pozdrawiam.');
+console.log(`Nazywam się ${name} i mam ${age} lata.`);
